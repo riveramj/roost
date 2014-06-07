57233b2a8e681a516eeea38b18a0e351c0912393
Justin Kronz <jkronz@gmail.com>
2014-06-06 23:46:16 -0400

    User Registration
    Make a new user form. Show error messages when passwords fail or
    whatever.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
   before_filter :require_current_user, only: [:me]
   def create
-    @user = User.create(params)
+    @user = User.create(user_params)
+    unless @user.errors.present?
+      redirect_to user_profile_path
+    else
+      render action: :new
+    end
   end
 
   def me
     @user = current_user
   end
 
+  def new
+    @user = User.new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
