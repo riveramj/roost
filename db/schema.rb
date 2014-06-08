ad9e6fdc2897b41bd49c1f04d573772ec04c9302
Mike Rivera <rivera.mj@gmail.com>
2014-06-08 00:37:26 -0400

    more styling for view listing

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140607222733) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: true do |t|
     t.string   "street1"
     t.string   "street2"
